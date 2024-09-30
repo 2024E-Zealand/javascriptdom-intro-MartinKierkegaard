0b306ffb01ec6a9244cfbe40293e693fa9b63161
Martin Kierkegaard <MARK@zealand.dk>
2024-09-30 14:06:57 +0200

    tilføjet et nyt li element til ul elementet

--- js/list.js
+++ js/list.js
@@ -1,7 +1,24 @@
 
 console.log("Skriv dit Javascript her")
-let a = document.getElementsByTagName("li");
+let a = document.getElementById("one")
+
 console.log(a)
+
+// let list = document.getElementsByTagName("li")
+
+//  console.log(list)
+
+let newLielement = document.createElement("li")
+ newLielement.setAttribute("class","hot")
+
+ let newTextelement = document.createTextNode("Apple")
+
+ newLielement.append(newTextelement)
+
+
+document.getElementById("firstul").append(newLielement)
+
+
 
 
 console.log("slut på JavaScript")
